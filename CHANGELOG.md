db34d88b902545f7367abea13f92789d5a3a4d78
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2019-05-22 15:47:44 +0700

    Adding CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# ChangeLog
+
+## Version 1.1.0
+
+1. Adding the `load.koolreport.php` to easily configure path for koolreport's library
--- load.koolreport.php
+++ load.koolreport.php
@@ -4,7 +4,7 @@
  * If you install koolreport with composer, you only need to
  * require the composer autoload file
  * 
- * require_once "/path/to/vendor/autoload.php";
+ * require_once dirname(__FILE__)."/vendor/autoload.php";
  */
 
 require_once dirname(__FILE__)."/../koolreport/core/autoload.php";
