a53481e8e4a71e6f9edded0f41797d6140c265e7
dongnl <nguyenluongdong@gmail.com>
2019-11-12 15:35:24 +0700

    update to use sample db

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 return array(
     "automaker"=>array(
-        "connectionString"=>"mysql:host=localhost;dbname=automaker",
-        "username"=>"root",
-        "password"=>"",
+        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=automaker",
+        "username"=>"expusr",
+        "password"=>"koolreport sampledb",
         "charset"=>"utf8"
     ),
     "sakila"=>array(
-        "connectionString"=>"mysql:host=localhost;dbname=sakila",
-        "username"=>"root",
-        "password"=>"",
+        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=sakila",
+        "username"=>"expusr",
+        "password"=>"koolreport sampledb",
         "charset"=>"utf8"
     ),
     "world"=>array(
-        "connectionString"=>"mysql:host=localhost;dbname=world",
-        "username"=>"root",
-        "password"=>"",
+        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=world",
+        "username"=>"expusr",
+        "password"=>"koolreport sampledb",
         "charset"=>"utf8"
     ),  
     "employees"=>array(
-        "connectionString"=>"mysql:host=localhost;dbname=employees",
-        "username"=>"root",
-        "password"=>"",
+        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=employees",
+        "username"=>"expusr",
+        "password"=>"koolreport sampledb",
         "charset"=>"utf8"
     ),  
     "salesCSV"=>array(
