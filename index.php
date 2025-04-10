f01a9cab0dc459cf56819d8092406b12dd29fbd1
dongnl <nguyenluongdong@gmail.com>
2025-04-10 11:35:16 +0700

    add expand/collapse all button on index.php

--- index.php
+++ index.php
@@ -33,6 +33,24 @@ include "helpers/common.php";
         i.classList.toggle('fa-plus-square-o');
         i.classList.toggle('fa-minus-square-o');
     }
+
+    function toggleExpandCollapseAll(i) {
+        let div = i.parentElement.parentElement;
+        let expandCollapseButtons = div.querySelectorAll('.expand-collapse');
+        expandCollapseButtons.forEach(function(btn) {
+            if (i.classList.contains('fa-plus-square-o')) {
+                if (btn.classList.contains('fa-plus-square-o')) {
+                    btn.click();
+                }
+            } else if (i.classList.contains('fa-minus-square-o')) {
+                if (btn.classList.contains('fa-minus-square-o')) {
+                    btn.click();
+                }
+            }
+        })
+        i.classList.toggle('fa-plus-square-o');
+        i.classList.toggle('fa-minus-square-o');
+    }
 </script>
 <body>
     <?php include "helpers/nav.php"; ?>
@@ -59,11 +77,21 @@ include "helpers/common.php";
                 <?php
                 foreach($section as $group_name=>$group)
                 {
+                    $hasChildArray = false;
+                    foreach($group as $sname=>$surl) {
+                        if(is_array($surl)) {
+                            $hasChildArray = true;
+                            break;
+                        }
+                    }
                 ?>
                     <div class="col-md-3 example-group col-sm-6">
                         <h5>
                         <!-- <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> -->
-                        <?php echo (strpos($group_name,"</i>")>0)?$group_name:"<i class='icon-layers'></i>$group_name"; ?></h5>
+                            <?php if ($hasChildArray) { ?>
+                                <i class='fa fa-plus-square-o' data-toggle="collapse" onclick="toggleExpandCollapseAll(this);"></i>
+                            <?php } ?>
+                            <?php echo (strpos($group_name,"</i>")>0)?$group_name:"<i class='icon-layers'></i>$group_name"; ?></h5>
                         <ul class="list-unstyled">
                         <?php
                         // echo "group: "; print_r($group);
@@ -85,7 +113,7 @@ include "helpers/common.php";
                             ?>
                                 <li>
                                 <!-- <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> -->
-                                <strong><i class='fa fa-plus-square-o' data-toggle="collapse" data-target="#<?php echo $idName; ?>" onclick="toggleExpandCollapse(this);"></i> <?php echo $sname; ?></strong>
+                                <strong><i class='fa fa-plus-square-o expand-collapse' data-toggle="collapse" data-target="#<?php echo $idName; ?>" onclick="toggleExpandCollapse(this);"></i> <?php echo $sname; ?></strong>
                                     <ul class="list-unstyled collapse" id="<?php echo $idName; ?>">
                                     <?php
                                     foreach($surl as $tname=>$turl)
